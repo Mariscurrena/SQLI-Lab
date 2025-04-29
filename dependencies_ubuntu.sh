d461af9ffbcd91f46ea18ea9bd8619f23d99fbcc
steve <mariscurrenalae@gmail.com>
2025-04-29 13:53:42 -0600

    Added dependencies_ubuntu.sh script

---
+++ dependencies_ubuntu.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+clear
+
+### Tkinter
+echo "Installing Tkinter..."
+sudo apt install python3-tk -y
+
+### Pillow
+echo "Installing Pillow"
+sudo apt install python3-pil
+sudo apt install python3-pil.imagetk
+
+### pymysql
+echo "Installing pymysql"
+sudo apt install python3-pymysql
